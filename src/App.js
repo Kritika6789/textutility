019c6aecf7ff1a388a7ecf9422be4e04d8bb26ae
kritika6789 <kritikajain1805@gmail.com>
2023-07-24 11:33:34 +0530

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import './App.css';
+import About from './component/About';
+import Alert from './component/Alert';
 import Navbar from './component/Navbar.js';
 import TextForm from './component/TextForm';
-
+import React, {useState} from 'react';
 function App() {
+  
+ const[mode, setMode]=useState('light');
+//  const[alert, setAlert]=useState(null);
+const[alert,setAlert]=useState(null);
+const showAlert=(message,type)=>{
+   setAlert({
+    message:message,
+    type:type
+   })
+   setTimeout(()=>{
+    setAlert(null);
+   },1500);
+}
+
+ const toMode=()=>{
+  if(mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor="#212529";
+    document.body.style.color="white";
+    showAlert("Dark mode is enabled","success");
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+    showAlert("Light mode is enabled","success");
+    
+    
+  }
+
+ }
+ 
   return (
  <>
- <Navbar title="TextUtility" AboutText="About Text"/> 
+ <Navbar title="TextUtility" AboutText="About Text" mode={mode} toggle={toMode}  /> 
+ <Alert alert={alert} /> 
  <div className="container my-3">
- <TextForm  heading="Enter the text to analyze"/>
+  <TextForm  showAlert={showAlert} heading="Enter the text to analyze"  mode={mode}/>   
+ <About />
+ 
  </div>
 
  </>
